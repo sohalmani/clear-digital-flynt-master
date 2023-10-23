f2df886ddda2226a77df921e7a65d79ddeb9575f
Chander Shekhar <chander@webenertia.com>
2023-10-23 19:38:03 +0530

    fix: nav logo

--- Components/NavigationMain/script.js
+++ Components/NavigationMain/script.js
@@ -1,6 +1,7 @@
 export default function (el) {
   const isDesktopMediaQuery = window.matchMedia('(min-width: 1024px)')
   isDesktopMediaQuery.addEventListener('change', onBreakpointChange)
+  window.addEventListener('scroll', handleHeaderScroll)
 
   onBreakpointChange()
 
@@ -15,5 +16,13 @@ export default function (el) {
       ? document.getElementById('wpadminbar').offsetHeight
       : 0
     document.documentElement.style.scrollPaddingTop = `${scrollPaddingTop}px`
+  }
+
+  function handleHeaderScroll () {
+    if (window.scrollY > 0) {
+      el.classList.add('navigation-isScrolled')
+    } else {
+      el.classList.remove('navigation-isScrolled')
+    }
   }
 }
